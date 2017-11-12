dcb1ce352f130e1e1045ab80e360a9310e6bf2fe
tmcgroul <safonov.a.r@mail.ru>
2017-11-13 02:36:14 +0300

    add algorithm for change long_url

---
+++ urlshorter/urlshort.py
@@ -0,0 +1,32 @@
+from .models import Url
+
+ALPHABET = "abcdefghijklmnopqrstuvwxuzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+BASE = 62
+
+def add_shorted_url(long_url):
+    Url.objects.all()
+    add_url = Url(URL=long_url, Code="")
+    add_url.save()
+    add_url.Code = encode(add_url.id)
+    add_url.save()
+
+def encode(num):
+    sb = ""
+    while num > 0:
+        sb += ALPHABET[(num % BASE)]
+        num = int(num / BASE)
+
+    builder = ""
+    for i in range(len(sb)):
+        builder = sb[i] + builder
+
+    return builder
+
+
+def decode(str):
+    num = 0
+    for i in range(len(str)):
+        num = num * BASE + ALPHABET.index(str[i])
+
+    return num
+
--- urlshorter/views.py
+++ urlshorter/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .forms import UrlsForm
+from .urlshort import add_shorted_url
 
 def mainpage(request):
     form = UrlsForm(request.POST or None)
     if request.method == "POST":
-        print(request.POST)
-        new_form = form.save()
+        url = request.POST.get("URL")
+        add_shorted_url(url)
+
+        ##new_form = form.save()
 
     return render(request, 'urlshorter/mainpage.html', locals())
