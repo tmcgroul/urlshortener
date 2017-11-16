a0a993bcb08317bb7c93c37a244a07f99ef614e0
tmcgroul <safonov.a.r@mail.ru>
2017-11-17 02:32:48 +0300

    add regex to url template

--- urlshorter/urls.py
+++ urlshorter/urls.py
@@ -2,6 +2,8 @@ from urlshorter import views
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^mainpage', views.mainpage, name = 'mainpage'),
-    url(r'^djopa6', views.trans_to_shorturl, name='redirect'),
+    url(r'^mainpage', views.mainpage),
+    #url(r'^[0-9]', views.trans_to_shorturl),
+    url(r'^(?P<shortcode>[\w-]+)/$', views.trans_to_shorturl),
+    #url(r'^[0-5]', views.trans_to_shorturl),
 ]
--- urlshorter/views.py
+++ urlshorter/views.py
@@ -13,9 +13,11 @@ def mainpage(request):
         ##new_form = form.save()
     return render(request, 'urlshorter/mainpage.html', locals())
 
-def trans_to_shorturl(self):
+def trans_to_shorturl(self, shortcode=None):
     #getting long_url by filtered "Code"
-    long_url = Url.objects.get(Code='http://127.0.0.1:8000/3');
+    shortcode = 'http://127.0.0.1:8000/' + shortcode
+    print(shortcode)
+    long_url = Url.objects.get(Code=shortcode);
     print(long_url.Code)
     return redirect(long_url.URL)
 
