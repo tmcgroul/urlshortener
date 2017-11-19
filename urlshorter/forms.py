6a08f6d1858801dcf5371edcb50c06ab86e12e97
tmcgroul <safonov.a.r@mail.ru>
2017-11-19 15:56:47 +0300

    add input_Code on html

--- urlshorter/forms.py
+++ urlshorter/forms.py
@@ -6,3 +6,7 @@ class UrlsForm(forms.ModelForm):
         class Meta:
             model = Url
             exclude = [""]
+
+        def __init__(self, *args, **kwargs):
+            super(UrlsForm, self).__init__(*args, **kwargs)
+            self.fields['Code'].widget.attrs['readonly'] = True
